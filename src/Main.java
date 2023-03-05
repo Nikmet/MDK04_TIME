acff528f32031024f1af06484527fb62a3925e26
Nikmet <metlov.n@mail.ru>
2023-03-05 16:04:00 +0300

    pisapopa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String answer = "";
+        System.out.print("Введите строку со временем: ");
+        String text = sc.nextLine();
+        Pattern pattern = Pattern.compile("\\d{2}:\\d{2}");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            answer = (text.substring(matcher.start(), matcher.end()));
+        }
+        if (Integer.parseInt(answer.substring(0, 2)) > 23 || Integer.parseInt(answer.substring(3, 4)) > 59) {
+            System.out.println("Неправильное время");
+        } else {
+            System.out.println(answer);
+        }
     }
 }
