7a32be7ca21d3eb8006f5e4f09579b06649f0075
Nikmet <metlov.n@mail.ru>
2023-03-05 16:06:19 +0300

    all

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String answer = "";
+        String time = new String();
         System.out.print("Введите строку со временем: ");
         String text = sc.nextLine();
         Pattern pattern = Pattern.compile("\\d{2}:\\d{2}");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
-            answer = (text.substring(matcher.start(), matcher.end()));
+            time = (text.substring(matcher.start(), matcher.end()));
         }
-        if (Integer.parseInt(answer.substring(0, 2)) > 23 || Integer.parseInt(answer.substring(3, 4)) > 59) {
+        if (Integer.parseInt(time.substring(0, 2)) > 23 || Integer.parseInt(time.substring(3, 4)) > 59) {
             System.out.println("Неправильное время");
         } else {
-            System.out.println(answer);
+            System.out.println(time);
         }
     }
 }
